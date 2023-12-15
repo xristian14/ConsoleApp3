cc23f66cda0112eea96bd3aea5ec96f26ad54e63
Христиан <xristian1414@mail.ru>
2023-12-15 20:22:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = Int32.Parse(a);
+            int c = 1;
+            for(int i = 1; i <= b; i++)
+            {
+                c = c * i;
+            }
+            Console.WriteLine(b + "! = " + c);
+            Console.ReadLine();
+        }
+    }
+}
